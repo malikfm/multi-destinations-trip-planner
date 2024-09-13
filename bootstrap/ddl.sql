3bee633f2df3b3d47549453c33459886db610ff9
malikfm <mubarok.malikfajar@gmail.com>
2024-09-13 16:43:58 +0700

    finalize bootstrap

--- bootstrap/ddl.sql
+++ bootstrap/ddl.sql
@@ -1,24 +1,24 @@
 CREATE TABLE IF NOT EXISTS hotels (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255),
-    latitude FLOAT,
-    longitude FLOAT
+    longitude FLOAT,
+    latitude FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS tourism_spots (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255),
-    latitude FLOAT,
-    longitude FLOAT
+    longitude FLOAT,
+    latitude FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS tags (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS tourism_spots_tags (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     tourism_spot_id INTEGER,
     tag_id INTEGER,
     FOREIGN KEY(tourism_spot_id) REFERENCES tourism_spots(id),
