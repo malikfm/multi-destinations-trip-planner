498e332834c3326fc3bf538b5538df410732ed6f
malikfm <mubarok.malikfajar@gmail.com>
2024-09-16 14:13:50 +0700

    feat: add vercel configuration

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 from os import environ as env
 
-
 SQLITE_DB_PATH = env.get("SQLITE_DB_PATH", "multi_destinations_trip_planner.db")
--- trip_repository.py
+++ trip_repository.py
@@ -1,6 +1,6 @@
 import sqlite3
 from contextlib import closing
-from typing import List, Tuple, Optional
+from typing import List, Optional, Tuple
 
 import config
 
@@ -18,18 +18,24 @@ class TripRepository:
 
     def get_hotel_by_id(self, hotel_id: int) -> Optional[sqlite3.Row]:
         with closing(self.conn.cursor()) as cursor:
-            cursor.execute("SELECT id, name, latitude, longitude FROM hotels WHERE id = ?", (hotel_id,))
+            cursor.execute(
+                "SELECT id, name, latitude, longitude FROM hotels WHERE id = ?",
+                (hotel_id,),
+            )
             return cursor.fetchone()
 
     def get_tourism_spots_by_tag(self, tag: str) -> List[sqlite3.Row]:
         with closing(self.conn.cursor()) as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 SELECT ts.id, ts.name, ts.latitude, ts.longitude
                 FROM tourism_spots ts
                 JOIN tourism_spots_tags tst ON ts.id = tst.tourism_spot_id
                 JOIN tags t ON tst.tag_id = t.id
                 WHERE t.name = ?
-                """, (tag,))
+                """,
+                (tag,),
+            )
             return cursor.fetchall()
 
     def get_tags(self) -> List[sqlite3.Row]:
